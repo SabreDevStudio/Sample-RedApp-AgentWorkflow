f5c82e721831f90c7fe26d1d601aa9c30be82d1f
Meneghello <sg0207087@global.ad.sabre.com>
2018-04-07 09:08:05 -0300

    sync removed classes

--- com.sabre.tn.redapp.example.workflow/src/com/sabre/tn/redapp/example/workflow/xtpointservices/IWorkflowXTService.java
+++
@@ -1,9 +0,0 @@
-package com.sabre.tn.redapp.example.workflow.xtpointservices;
-
-import com.sabre.stl.pos.srw.nextgen.flow.ext.v2.FlowExtPointCommand;
-
-public interface IWorkflowXTService {
-
-	FlowExtPointCommand execute(FlowExtPointCommand extPointCommand);
-
-}
